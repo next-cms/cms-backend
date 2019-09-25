5fb651b1362e5e48af16c15aa18a3fd191dd4f4b
tahsinsiad <tak.siad16@gmail.com>
2019-09-25 12:35:51 +0600

    page creation path problem fixed

--- src/generators/PageGenerator.ts
+++ src/generators/PageGenerator.ts
@@ -27,7 +27,9 @@ async function getFileName(dirPath) {
 }
 
 export async function addNewPage(projectId): Promise<Page> {
-    const templateFilePath = path.join(__dirname, '../../templates', 'BlankPage.js');
+    console.log("Project Id: ", projectId);
+    const templateFilePath = path.join(__dirname, 'BlankPage.js.template');
+    console.log("DIr name: ", __dirname);
     return await fsp.readFile(templateFilePath, 'utf8')
         .then(async (templateFile) => {
             const fileNameID = await getFileName(path.join(PROJECT_ROOT, projectId, PROJECT_FRONTEND, 'pages'));
